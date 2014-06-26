920ec424cf24e38fbddf848fb605311e91d14543
Administrator <Administrator@PC-20140526XWZZ>
2014-06-26 10:49:27 +0800

    update by zj

--- src/com/wantdo/test.java
+++ src/com/wantdo/test.java
@@ -7,7 +7,7 @@ public class test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWord");
+		System.out.println("Welcome");
 
 	}
 

b9a4deb21bcb082932f2fbd30505cece5ed1e111
Administrator <Administrator@PC-20140526XWZZ>
2014-06-26 09:49:28 +0800

    add item by zj

---
+++ src/com/wantdo/test.java
@@ -0,0 +1,14 @@
+package com.wantdo;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWord");
+
+	}
+
+}
